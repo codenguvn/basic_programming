0b01b4d1d4a59930c58605ba0f1ff714f55e3c68
Code NGU <codengu@Lans-MacBook-Pro.local>
2023-05-27 13:39:52 +0200

    variable 101

---
+++ 01.variables/variable_101.py
@@ -0,0 +1,13 @@
+# Copyright of CodeNgu
+
+ageOfHandsomeA = 16
+print("Age of handsome A is: ", ageOfHandsomeA)
+
+ageOfHandsomeA = ageOfHandsomeA + 2
+print("Age of handsome A next 2 years: ", ageOfHandsomeA)
+
+ageOfHandsomeA += 4
+print("Age of handsome A next 4 years: ", ageOfHandsomeA)
+
+ageOfHandsomeA -= 6
+print("The current age of A is:", ageOfHandsomeA)
