c7f449a22f17d1f3337f6c5b6d7191acb1c5aee7
Code NGU <codengu@Lans-MacBook-Pro.local>
2023-06-02 23:38:58 +0200

    constants 101

---
+++ 02.constants/constant_101/constant_101.js
@@ -0,0 +1,9 @@
+// chu vi hinh tron C = 2 * pi * r
+let radius = 1;
+console.log("value of radius is: ", radius);
+
+const numberPI = 3.14;
+console.log("value of numberPI is: ", numberPI);
+
+let circumference = 2 * numberPI * radius;
+console.log("value of circumference is: ", circumference);
